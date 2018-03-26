f9c75b5bd51c52b9be7df7611489cce23abc7585
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2018-03-26 17:38:32 +0700

    clean up

--- application/views/pages/ui.php
+++ application/views/pages/ui.php
@@ -1,25 +1,40 @@
-<button class="tombol kecil">tombol kecil</button>
-<button class="tombol">Tombol</button>
-<button class="tombol besar">tombol besar</button>
+<div>
+    <button class="tombol kecil">tombol kecil</button>
+    <button class="tombol">Tombol</button>
+    <button class="tombol besar">tombol besar</button>
+</div>
 
-<button class="tombol kecil lebar">tombol kecil lebar</button>
-<button class="tombol lebar">tombol lebar</button>
-<button class="tombol besar lebar">tombol besar lebar</button>
+<div>
+    <button class="tombol kecil lebar">tombol kecil lebar</button>
+    <button class="tombol lebar">tombol lebar</button>
+    <button class="tombol besar lebar">tombol besar lebar</button>
+</div>
 
-<button class="tombol kecil penuh">tombol kecil penuh</button>
-<button class="tombol penuh">tombol penuh</button>
-<button class="tombol besar penuh">tombol besar penuh</button>
+<div>
+    <button class="tombol kecil penuh">tombol kecil penuh</button>
+    <button class="tombol penuh">tombol penuh</button>
+    <button class="tombol besar penuh">tombol besar penuh</button>
+</div>
 
-<div class="tombol kecil">tombol kecil</div>
-<div class="tombol ">tombol </div>
-<div class="tombol besar">tombol besar</div>
+<div>
+    <div class="tombol kecil">tombol kecil</div>
+    <div class="tombol ">tombol </div>
+    <div class="tombol besar">tombol besar</div>
+</div>
 
-<a href="#" class="tombol besar">Link</a>
-<div class="tombol ">tombol </div>
-<div class="tombol merah">tombol </div>
-<div class="tombol kuning">tombol </div>
-<div class="tombol biru">tombol </div>
-<div class="tombol hijau">tombol </div>
-<div class="tombol putih">tombol </div>
-<div class="tombol hitam">tombol </div>
-<div class="tombol kelabu">tombol </div>
+<div>
+    <a href="#" class="tombol besar">Link</a>
+</div>
+
+<div>
+    <div class="tombol ">tombol </div>
+    <div class="tombol merah">tombol </div>
+    <div class="tombol kuning">tombol </div>
+    <div class="tombol biru">tombol </div>
+    <div class="tombol hijau">tombol </div>
+    <div class="tombol putih">tombol </div>
+    <div class="tombol hitam">tombol </div>
+    <div class="tombol kelabu">tombol </div>
+</div>
+
+
