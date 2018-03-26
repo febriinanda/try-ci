11f09f173a4b7a7555b6f4d6f0e5fa435acab337
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2018-03-26 17:23:04 +0700

    tombol berwarna

---
+++ application/views/pages/ui.php
@@ -0,0 +1,25 @@
+<button class="tombol kecil">tombol kecil</button>
+<button class="tombol">Tombol</button>
+<button class="tombol besar">tombol besar</button>
+
+<button class="tombol kecil lebar">tombol kecil lebar</button>
+<button class="tombol lebar">tombol lebar</button>
+<button class="tombol besar lebar">tombol besar lebar</button>
+
+<button class="tombol kecil penuh">tombol kecil penuh</button>
+<button class="tombol penuh">tombol penuh</button>
+<button class="tombol besar penuh">tombol besar penuh</button>
+
+<div class="tombol kecil">tombol kecil</div>
+<div class="tombol ">tombol </div>
+<div class="tombol besar">tombol besar</div>
+
+<a href="#" class="tombol besar">Link</a>
+<div class="tombol ">tombol </div>
+<div class="tombol merah">tombol </div>
+<div class="tombol kuning">tombol </div>
+<div class="tombol biru">tombol </div>
+<div class="tombol hijau">tombol </div>
+<div class="tombol putih">tombol </div>
+<div class="tombol hitam">tombol </div>
+<div class="tombol kelabu">tombol </div>
