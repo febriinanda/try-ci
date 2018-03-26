21f5d308a86b1e6dacdd6edad77288b02e7ec6b1
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2018-03-26 16:00:08 +0700

    set up news and css

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -25,9 +25,33 @@ class News extends CI_Controller{
 
         $data['title'] = $data['news_item']['title'];
 
-        $this->load->view('templates/header', $data);
+        $this->load->view('template/header', $data);
         $this->load->view('news/view', $data);
-        $this->load->view('templates/footer');
+        $this->load->view('template/footer');
+    }
+
+    public function create()
+    {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        $data['title'] = 'Create a news item';
+
+        $this->form_validation->set_rules('title', 'Title', 'required');
+        $this->form_validation->set_rules('text', 'Text', 'required');
+
+        if ($this->form_validation->run() === FALSE)
+        {
+            $this->load->view('template/header', $data);
+            $this->load->view('news/create');
+            $this->load->view('template/footer');
+
+        }
+        else
+        {
+            $this->news_model->set_news();
+            $this->load->view('news/success');
+        }
     }
 }
 ?>
