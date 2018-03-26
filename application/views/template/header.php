71ce65dcb9268ab0c7ecaa0448e673c4cff9b8a2
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2018-03-26 16:43:35 +0700

    button size

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>public/css/style.css"
+    <link rel="stylesheet" href="<?php echo base_url(); ?>public/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>public/css/app.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Try Codeigniter</title>
 </head>
